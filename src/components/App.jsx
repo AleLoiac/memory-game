3b3d03da9c68999244618d723205e9628c338215
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-07-28 19:28:20 +0200

    Set video background

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,16 @@
 import "../styles/App.css";
 
-export default function App() {}
+export default function App() {
+  const pokemonBackground = "../src/assets/video/pokemon-emerald-waterfall.mp4";
+
+  return (
+    <video
+      src={pokemonBackground}
+      className="bgVideo"
+      autoPlay
+      muted
+      loop
+      type="video/mp4"
+    ></video>
+  );
+}
